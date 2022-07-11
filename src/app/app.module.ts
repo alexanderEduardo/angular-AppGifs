c9a2e3c4f74469c3a93f5599504d8e3f2a49435b
Alexander <alexdev0411@gmail.com>
2022-07-11 09:14:27 -0500

    agrego el modulo http y testeo la api de gifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey:string = "6S1CiUqOZaI9VE21yGtM67fMhAmclfdv";
   private _historial:string[]=[];
+  private resultados:any[]=[];
+
+  constructor(private http: HttpClient){}
 
   get historial(){
     return [...this._historial];
@@ -19,5 +24,7 @@ export class GifsService {
     this._historial.unshift(query);
     this._historial=this._historial.splice(0,10);
     console.log(this._historial);
+    this.http.get<any>(`https://api.giphy.com/v1/gifs/search?api_key=6S1CiUqOZaI9VE21yGtM67fMhAmclfdv&q=${query}&limit=10`)
+    .subscribe( res => console.log(res.data))
   }
 }
