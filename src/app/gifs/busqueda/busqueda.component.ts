3cb8879364c55e3b271608c21ddf804f58b59d6d
Alexander <alexdev0411@gmail.com>
2022-07-10 23:00:22 -0500

    he implementado validaciones en el input -es funcinal end

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -11,9 +12,13 @@ export class BusquedaComponent {
   @ViewChild("txtBuscar")
   txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor(private gifService:GifsService){}
+
   buscar(parm:HTMLInputElement){
+    let value_ = parm.value;
+    if(value_.trim().length===0){return}
+    this.gifService.buscarGifs(value_);
     console.log(this.txtBuscar.nativeElement.value);
-    console.log(parm.value);
     //parm.value="";
     this.txtBuscar.nativeElement.value="";
   }
