924328c91140a1573e00199e3b1d2ea9a09d5f69
Alexander <alexdev0411@gmail.com>
2022-07-12 18:55:26 -0500

    new font JetBrains

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,28 +1,30 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GifsService } from '../services/gifs.service';
 import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
 export class BusquedaComponent {
-
+ 
   @ViewChild("txtBuscar")
   txtBuscar!:ElementRef<HTMLInputElement>;
 
   constructor(private gifService:GifsService){}
 
-  buscar(parm:HTMLInputElement){
+  buscar(parm:HTMLInputElement) {
     let value_ = parm.value;
-    if(value_.trim().length===0){return}
+    if (value_.trim().length === 0) {return}
     this.gifService.setSideBarItems(value_);
     console.log(this.txtBuscar.nativeElement.value);
-    //parm.value="";
-    this.txtBuscar.nativeElement.value="";
+    //parm.value = "";
+    this.txtBuscar.nativeElement.value = "";
+    
   }
 
-  showAuthor(){
+  showAuthor() {
     Swal.fire('El creador de este proyecto ha sido Alexander Peña 😃');
   }
 }
